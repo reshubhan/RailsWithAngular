c921a38175fbf4329d56b82cf71510eb83bfba37
Amar Singh Kaithwar <amar_kaithwar@intuit.com>
2017-01-12 16:34:12 +0530

    Added link as part of form submit

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ var app = angular.module("flapperNews", [])
 app.controller('MainCtrl', function($scope) {
     $scope.test= "Hello World !";
     $scope.posts = [
-    					{"title" : "Post 1", "upvote" : 9},
-    					{"title" : "Post 2", "upvote" : 4},
-    					{"title" : "Post 3", "upvote" : 5},
-    					{"title" : "Post 4", "upvote" : 4},
-    					{"title" : "Post 5", "upvote" : 8} 
+    					{"title" : "Post 1", "upvote" : 9, "link": "http://google.com"},
+    					{"title" : "Post 2", "upvote" : 4, "link": "http://google.com"},
+    					{"title" : "Post 3", "upvote" : 5, "link": ""},
+    					{"title" : "Post 4", "upvote" : 4, "link": "http://google.com"},
+    					{"title" : "Post 5", "upvote" : 8, "link": ""} 
     				]
     $scope.addPost = function(){
     	if (!$scope.title || $scope.title == ""){
     		console.log($scope.title);
     		return;
     	}
-    	$scope.posts.push({'title' : $scope.title, "upvote" : 0});
+    	$scope.posts.push({'title' : $scope.title, 'link': $scope.link ,"upvote" : 0});
     	$scope.title = "";
+    	$scope.link  = "";
     };
-    
+
     $scope.incrementUpvotes = function(post){
     	post.upvote += 1;
     }
