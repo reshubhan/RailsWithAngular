5c243d575844414d767a5abb6c2e5bca36607693
Amar Singh Kaithwar <amar_kaithwar@intuit.com>
2017-01-12 16:43:39 +0530

    Added comment in angular js function

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ var app = angular.module("flapperNews", [])
 
 app.controller('MainCtrl', function($scope) {
     $scope.test= "Hello World !";
+
+    //List of Posts
     $scope.posts = [
     					{"title" : "Post 1", "upvote" : 9, "link": "http://google.com"},
     					{"title" : "Post 2", "upvote" : 4, "link": "http://google.com"},
     					{"title" : "Post 3", "upvote" : 5, "link": ""},
     					{"title" : "Post 4", "upvote" : 4, "link": "http://google.com"},
     					{"title" : "Post 5", "upvote" : 8, "link": ""} 
+    
     				]
+
+    //Function to add new post	
     $scope.addPost = function(){
     	if (!$scope.title || $scope.title == ""){
     		console.log($scope.title);
@@ -19,6 +24,7 @@ app.controller('MainCtrl', function($scope) {
     	$scope.link  = "";
     };
 
+    //Function to  increment votes
     $scope.incrementUpvotes = function(post){
     	post.upvote += 1;
     }
