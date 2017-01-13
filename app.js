d86a9253a5b3247838a71e7d09591e6f0ba659a6
Amar Singh Kaithwar <amar_kaithwar@intuit.com>
2017-01-13 11:06:43 +0530

    Added a service as factory for posts object

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var app = angular.module("flapperNews", [])
 
-app.controller('MainCtrl', function($scope) {
-    $scope.test= "Hello World !";
-
-    //List of Posts
-    $scope.posts = [
+app.factory("posts", [function(){
+	var o = {
+		posts:  [
     					{"title" : "Post 1", "upvote" : 9, "link": "http://google.com"},
     					{"title" : "Post 2", "upvote" : 4, "link": "http://google.com"},
     					{"title" : "Post 3", "upvote" : 5, "link": ""},
     					{"title" : "Post 4", "upvote" : 4, "link": "http://google.com"},
     					{"title" : "Post 5", "upvote" : 8, "link": ""} 
     
-    				]
+    			]
+	};
+	return o;
+}]);
 
+
+app.controller('MainCtrl',['$scope', 'posts', function($scope, posts) {
+    $scope.test= "Hello World !";
+    $scope.posts = posts.posts
+    console.log(posts);
     //Function to add new post	
     $scope.addPost = function(){
     	if (!$scope.title || $scope.title == ""){
@@ -29,4 +35,4 @@ app.controller('MainCtrl', function($scope) {
     	post.upvote += 1;
     }
     
-});
+}]);
