24c12fb47425699f7ec2e568c4f4c39b6f40c9e8
Amar Singh Kaithwar <amar_kaithwar@intuit.com>
2017-01-16 16:16:15 +0530

    Added single post view and state to app

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-var app = angular.module("flapperNews", [])
+var app = angular.module("flapperNews", ['ui.router'])
 
+
+//Routes and configurations starts here 
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	$stateProvider
+	.state('home',{
+		url: '/home',
+		templateUrl: '/home.html',
+		controller: 'MainCtrl'
+	})
+	.state('posts',{
+		url: '/posts/{id}',
+		templateUrl: '/posts.html',
+		controller: 'PostsCtrl'
+	})
+
+	$urlRouterProvider.otherwise('home');
+
+}]);
+
+
+
+
+//Factory and service defination starts here
 app.factory("posts", [function(){
 	var o = {
 		posts:  [
-    					{"title" : "Post 1", "upvote" : 9, "link": "http://google.com"},
-    					{"title" : "Post 2", "upvote" : 4, "link": "http://google.com"},
-    					{"title" : "Post 3", "upvote" : 5, "link": ""},
-    					{"title" : "Post 4", "upvote" : 4, "link": "http://google.com"},
-    					{"title" : "Post 5", "upvote" : 8, "link": ""} 
+					{"title" : "Post 1", "upvote" : 9, "link": "http://google.com", 'comments': ['1','2']},
+					{"title" : "Post 2", "upvote" : 4, "link": "http://google.com", 'comments': ['1','2']},
+					{"title" : "Post 3", "upvote" : 5, "link": "", 'comments': ['1','2']},
+					{"title" : "Post 4", "upvote" : 4, "link": "http://google.com", 'comments': ['1','2']},
+					{"title" : "Post 5", "upvote" : 8, "link": "", 'comments': ['1','2']} 
     
     			]
 	};
@@ -15,17 +38,29 @@ app.factory("posts", [function(){
 }]);
 
 
+
+
+//Congtrollers definations starts here
 app.controller('MainCtrl',['$scope', 'posts', function($scope, posts) {
     $scope.test= "Hello World !";
     $scope.posts = posts.posts
     console.log(posts);
+
     //Function to add new post	
     $scope.addPost = function(){
     	if (!$scope.title || $scope.title == ""){
     		console.log($scope.title);
     		return;
     	}
-    	$scope.posts.push({'title' : $scope.title, 'link': $scope.link ,"upvote" : 0});
+    	$scope.posts.push({
+  			title: $scope.title,
+  			link:  $scope.link,
+  			upvote: 0,
+  			comments: [
+    			{author: 'Joe', body: 'Cool post!', upvote: 0},
+    			{author: 'Bob', body: 'Great idea but everything is wrong!', upvote: 0}
+  			]
+		});
     	$scope.title = "";
     	$scope.link  = "";
     };
@@ -36,3 +71,22 @@ app.controller('MainCtrl',['$scope', 'posts', function($scope, posts) {
     }
     
 }]);
+
+
+app.controller('PostsCtrl', ['$scope','posts','$stateParams', function($scope, posts, $stateParams){
+	console.log("testttttt"+JSON.stringify($stateParams));
+	$scope.posts = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+		if($scope.body == ''){
+			return;
+		}
+		$scope.post.comments.push({
+			comment_body: $scope.comment_body,
+			author: 'user',
+			upvote: 0
+		});
+		$scope.comment_body = '';
+	};
+
+}])
