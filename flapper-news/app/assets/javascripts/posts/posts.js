cc46f862074a364b7e32943d30b23b47f6c89409
Amar Singh Kaithwar <amar_kaithwar@intuit.com>
2017-01-23 17:05:05 +0530

    Integration done with backened

--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -1,15 +1,16 @@
 
 //Factory and service defination starts here
-app.factory("posts", [function(){
+app.factory("posts", ['$http', function($http){
 	var o = {
 		posts:  [
-					{"title" : "Post 1", "upvote" : 9, "link": "http://google.com", 'comments': [{author: 'Joe', comment_body: 'Cool post!', upvote: 0}]},
-					{"title" : "Post 2", "upvote" : 4, "link": "http://google.com", 'comments': [{author: 'Joe', comment_body: 'Cool post!', upvote: 0}]},
-					{"title" : "Post 3", "upvote" : 5, "link": "", 					'comments': [{author: 'Joe', comment_body: 'Cool post!', upvote: 0}]},
-					{"title" : "Post 4", "upvote" : 4, "link": "http://google.com", 'comments': [{author: 'Joe', comment_body: 'Cool post!', upvote: 0}]},
-					{"title" : "Post 5", "upvote" : 8, "link": "", 					'comments': [{author: 'Joe', comment_body: 'Cool post!', upvote: 0}]} 
-    
+					
     			]
 	};
+
+	o.getAll = function() {
+    	return $http.get('/posts.json').success(function(data){
+        	angular.copy(data, o.posts);
+    	});
+  	};
 	return o;
 }]);
